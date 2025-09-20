77725d627417b882723a2f6d58e0bd56cd02bb79
batch11devops <rakshashetty722@gmail.com>
2025-09-20 12:43:24 +0530

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,3 +1,6 @@
+module "vnet"{
+source = "github.com/batch11devops/ABC-20092025//modules/vnet?ref=main"
+
 resource "azurerm_virtual_network" "this" {
   name                = var.vnet_name
   address_space       = var.address_space
@@ -10,5 +13,6 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = var.subnet_prefixes
+}
 }
 
